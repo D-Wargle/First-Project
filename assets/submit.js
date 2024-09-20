d0a26f330404ef54abc2c40e74cca40721dc0759
Daniel T <Danieltwarg@gmail.com>
2024-09-19 22:10:54 -0400

    fixed basically everything on the page, css, and js functionality

--- assets/submit.js
+++ assets/submit.js
@@ -1,6 +1,4 @@
-const submitEl = document.querySelector('#UseriputForm');
-const trailEL = document.querySelector('#inputGroupSelector01');
-const ratingEL = document.querySelector('#inputGroupSelector02');
+const submitEl = document.querySelector('#userInputForm');
 
 // this is the function that will handle the form submission
 function handleFormSubmit(event) {
@@ -8,33 +6,31 @@ function handleFormSubmit(event) {
     
     const firstName = document.querySelector('#firstName-Box').value;
     const lastName = document.querySelector('#lastName-Box').value;
-    let e = document.getElementById("inputGroupSelector01");
-    let value = e.value;
+    let e = document.getElementById("inputGroupSelect01");
     let text = e.options[e.selectedIndex].text;
-    let e2 = document.getElementById("inputGroupSelector02");
-    let value2 = e2.value;
+    let e2 = document.getElementById("inputGroupSelect02");
     let text2 = e2.options[e2.selectedIndex].text;
     
-    if (!firstName || !lastName || !e|| !e2) {
+    if (!firstName || !lastName || !text|| !text2) {
         alert('You need to fill out the form');
         return;
     }
+
+    const allUserData ={
+        firstName,
+        lastName,
+        value: text,
+        value2: text2
+    }
+    
+    let storedUserInput = JSON.parse(localStorage.getItem('storedUserInput')) || []
+    
+    storedUserInput.push(allUserData)
+    
+    localStorage.setItem('storedUserInput', JSON.stringify(storedUserInput))
+    renderUserInput();
+    
 }
 
-//this is the object that will be sent to the server
-const allUserData ={
-    firstName,
-    lastName,
-    value: text,
-    value2: text2,
-};
-
-let storedUserInput = JSON.parse(localStorage.getItem('storedUserInput')) || [];
-
-storedUserInput.push(allUserData);
-
-localStorage.setItem('storedUserInput', JSON.stringify(storedUserInput));
-
-
-
 submitEl.addEventListener('submit', handleFormSubmit);
+
